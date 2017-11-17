74bc953316502412a32dadb5ef40275c4aa4bd1b
Anant Anand Gupta <anantanandgupta@gmail.com>
2017-11-17 15:36:29 +0530

    put life back to project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 define(["require", "exports", "./todoAppUI"], function (require, exports, ToDoAppUI) {
     "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
     ToDoAppUI.init();
 });
